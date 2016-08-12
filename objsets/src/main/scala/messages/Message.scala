269254fbf6b0c593b263e285e54c3e181a21ed9d
maan82 <maan82@rediffmail.com>
2016-08-12 08:22:37 +0100

    Messages

--- objsets/src/main/scala/messages/Message.scala
+++ objsets/src/main/scala/messages/Message.scala
@@ -1,10 +1,8 @@
 package messages
 
-class Message(sender:String, receiver:String,text:String){
-  def getSender() = sender
+class Message(text:String){
 
-  override def toString: String = "sender : " + getSender() +
-    " receiver : " + receiver +
+  override def toString: String =
     " text : " + text
 }
 
@@ -29,15 +27,39 @@ class NonEmptyMessageSet(elem: Message, next:MessageSet) extends MessageSet {
   override def toString = elem.toString + next.toString
 }
 
+abstract class MessageTree {
+  def filter(p:Message => Boolean) : MessageTree
+}
+
+class EmptyMessageTree extends MessageTree () {
+  override def toString = ""
+  override def filter(p:Message => Boolean) : MessageTree = new EmptyMessageTree
+}
+
+class NonEmptyMessageTree(elem:Message, left:MessageTree, right:MessageTree) extends MessageTree {
+
+  override def toString =  left.toString + " / " + elem.toString + " \\ " + right.toString
+
+  override def filter(p: (Message) => Boolean): MessageTree = {
+    if(p(elem)) {
+      new NonEmptyMessageTree(elem, left.filter(p), right.filter(p))
+    } else {
+      left.filter(p)
+    }
+  }
+
+}
+
 object Main{
   def main(args: Array[String]) {
-    val message1: Message = new Message("a", "b", "1 from a to b")
-    val message2: Message = new Message("a", "b", "2 from a to b")
-    val message3: Message = new Message("a", "b", "3 from a to b")
-    val message4: Message = new Message("4", "b", "4 from a to b")
+    val message1: Message = new Message("1")
+    val message2: Message = new Message("2")
+    val message3: Message = new Message("3")
+    val message4: Message = new Message("4")
 
     val messageSet: NonEmptyMessageSet = new NonEmptyMessageSet(message3, new NonEmptyMessageSet(message2, new NonEmptyMessageSet(message1, new EmptyMessageSet)))
 
-    println( messageSet.filter((p:Message) => { p.getSender() == "a" }))
+    //println( messageSet.filter((p:Message) => { p.getSender() == "a" }))
+    println(new NonEmptyMessageTree(message1,  new NonEmptyMessageTree(message2, new EmptyMessageTree, new EmptyMessageTree), new NonEmptyMessageTree(message3, new EmptyMessageTree, new EmptyMessageTree)))
   }
 }
